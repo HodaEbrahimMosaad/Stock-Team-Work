5be79309db5b5ef09460c7ed71379756dbe565e1
sorcerer <mahmoud.farouk987@gmail.com>
2019-02-14 00:39:14 +0200

    added PairPolicy

---
+++ app/Policies/PairPolicy.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Pair;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PairPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the pair.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Pair  $pair
+     * @return mixed
+     */
+    public function manage(User $user, Pair $pair)
+    {
+        $user->owns($pair);
+    }
+
+}
