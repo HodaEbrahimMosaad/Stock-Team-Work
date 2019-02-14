90b1bc8fb626ca21bd71f10007b1211dc5b1ff6a
sorcerer <mahmoud.farouk987@gmail.com>
2019-02-14 15:01:20 +0200

    added initial data file

---
+++ initialData.php
@@ -0,0 +1,22 @@
+<?php
+// currencies
+Currency::create(['currency_name'=>'USD']);
+Currency::create(['currency_name'=>'EGP']);
+Currency::create(['currency_name'=>'EUR']);
+Currency::create(['currency_name'=>'AFN']);
+Currency::create(['currency_name'=>'AUD']);
+Currency::create(['currency_name'=>'BHD']);
+Currency::create(['currency_name'=>'CAD']);
+Currency::create(['currency_name'=>'INR']);
+Currency::create(['currency_name'=>'IRR']);
+Currency::create(['currency_name'=>'IQD']);
+Currency::create(['currency_name'=>'SAR']);
+Currency::create(['currency_name'=>'TRY']);
+Currency::create(['currency_name'=>'AED']);
+Currency::create(['currency_name'=>'KWD']);
+
+
+//eventTypes
+EventType::create(['event_type_name'=>'less']);
+EventType::create(['event_type_name'=>'more']);
+
