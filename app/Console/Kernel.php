d41f5ec999a62a8acb98e8de5a351409f75bbf9c
sorcerer <mahmoud.farouk987@gmail.com>
2019-02-14 16:19:39 +0200

    added the task to be scheduled

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         //$schedule->command('inspire')->everyMinute();
+        $schedule->command('check:triggers')->hourly();
     }
 
     /**
