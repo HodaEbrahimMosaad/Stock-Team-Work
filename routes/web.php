2a1dbc1f220d760c2c2ff4a0fef2620c3ebf732b
Hoda Ibrhim <you@example.com>
2019-02-15 15:53:44 +0200

    edit pairs resource to 'pairs'

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,24 @@
 <?php
 
 
-
+use App\Jobs\SendEmail;
+use App\Mail\SendEmailMailable;
+use Illuminate\Support\Facades\Mail;
 
 Route::get('/', function (){return view('welcome');});
 
 // temp, to be deleted
 Route::get('/ho', 'HomeController@index');
 
-Route::resource('/home', 'PairController')->middleware('verified');
+Route::resource('/pairs', 'PairController')->middleware('verified');
+
+//Route::get('/home', 'PairController@index')->name('home');
+//Route::get('/pairs', 'PairController@getPairs')->name('pairs');
+
+
 Auth::routes(['verify' => true]);
+
+Route::get('sendEmail',function (){
+//    SendEmail::dispatch()->delay(now()->addSeconds(5));
+//   return 'done';
+});
